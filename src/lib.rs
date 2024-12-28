71b8288ca4cfcb1c013ec71598887ab64e5efb6f
Andrew Boessen <boessena@bc.edu>
2024-12-27 23:15:01 -0600

    structs

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,27 @@ extern "C" {
 pub fn greet() {
     alert("Hello, website!");
 }
+
+#[wasm_bindgen]
+#[repr(i8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Black = -1,
+    White = 1,
+}
+
+#[wasm_bindgen]
+pub struct Grid {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+pub struct Canvas {
+    grid_width: u32,
+    grid_height: u32,
+    width: u32,
+    height: u32,
+    grids: Vec<Grid>,
+}
