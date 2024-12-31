d3effa521969011fbd20695be8dd7159d6c0c725
Andrew Boessen <boessena@bc.edu>
2024-12-30 23:13:10 -0600

    javascript

---
+++ index.js
@@ -0,0 +1,48 @@
+import { Canvas, Cell } from "website";
+
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const NEGATIVE_COLOR = "#FFFFFF";
+const POSITIVE_COLOR = "#000000";
+
+// Construct the canvas, and get its width and height.
+const hopfield_canvas = Canvas.new();
+const width = hopfield_canvas.width();
+const height = hopfield_canvas.height();
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const renderLoop = () => {
+  hopfield_canvas.step();
+
+  drawGrid();
+  drawCells();
+
+  requestAnimationFrame(renderLoop);
+};
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
