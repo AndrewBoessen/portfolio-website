91cf454b6f75b8ad1dd7178908db5c040018a88a
Andrew Boessen <boessena@bc.edu>
2025-01-04 14:40:12 -0600

    fix webpack wasm support

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,40 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
+    webassemblyModuleFilename: "[hash].wasm",
+    publicPath: '/'
   },
   mode: "development",
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        type: "webassembly/async"
+      }
+    ]
+  },
   plugins: [
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, 'rust'),
       extraArgs: '--target bundler',
+      forceMode: "development"
     }),
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "index.html" }  // Updated to use new CopyWebpackPlugin syntax
+      ]
+    })
   ],
+  resolve: {
+    extensions: ['.js', '.wasm'],
+    fallback: {
+      fs: false,
+      path: false,
+      crypto: false
+    }
+  }
 };
